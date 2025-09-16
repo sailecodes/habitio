69642b426f468198418249a75d5851660364374a
saile <eliasiv.codes@gmail.com>
2025-09-15 17:41:53 -0700

    Implement initial home page layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <section>Home</section>;
+  return (
+    <section className="mx-auto flex w-full max-w-7xl justify-between gap-10 px-5 py-10">
+      <div className="flex-1/3">
+        <header className="mb-2 text-5xl font-bold">
+          Consistent habits with AI
+        </header>
+        <p className="mb-4 leading-relaxed">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates
+          eveniet ea, laborum rem ad nesciunt dolore inventore impedit nam
+          provident, ex, iure debitis ullam cupiditate molestias totam nostrum
+          delectus nemo.
+        </p>
+        <Button variant="secondary" className="hover:cursor-pointer">
+          Pricing
+        </Button>
+      </div>
+      <div className="flex-2/3"></div>
+    </section>
+  );
 }
