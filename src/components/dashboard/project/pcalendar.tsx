0d0dddcf4dd813cc28aab58e517910f1c773f181
saile <eliasiv.codes@gmail.com>
2025-09-19 12:23:14 -0700

    Implement initial project calendar view

--- src/components/dashboard/project/pcalendar.tsx
+++ src/components/dashboard/project/pcalendar.tsx
@@ -1,7 +1,28 @@
 "use client";
 
-import { useState } from 'react';
-import { Calendar } from '@/components/ui/calendar';
+import { useState } from "react";
+import { DayButton } from "react-day-picker";
+import { Calendar } from "@/components/ui/calendar";
+import { cn } from "@/lib/utils";
+
+function PCalendarDay({ day }: React.ComponentProps<typeof DayButton>) {
+  const key = day.date.toISOString().split("T")[0];
+  let style = "";
+
+  console.log(key);
+
+  if (key === new Date().toISOString().split("T")[0]) style = "";
+  if (key.includes("3")) style = "bg-green-400 text-white font-bold";
+  else if (key.includes("4")) style = "bg-orange-400 text-white font-bold";
+
+  return (
+    <div
+      className={cn("grid h-full w-full place-items-center rounded-md", style)}
+    >
+      {day.date.getDate()}
+    </div>
+  );
+}
 
 export default function PCalendar() {
   const [date, setDate] = useState<Date | undefined>(new Date());
@@ -12,8 +33,11 @@ export default function PCalendar() {
         mode="single"
         selected={date}
         onSelect={setDate}
+        components={{
+          DayButton: PCalendarDay,
+        }}
         className="w-full rounded-md"
-      ></Calendar>
+      />
     </div>
   );
 }
